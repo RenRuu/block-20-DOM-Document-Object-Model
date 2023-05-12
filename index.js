c579137a1857626dd70aef92c691ef04225e675c
“Rin” <renrust26@gmail.comgit config --global user.name Ringit config --global user.email renrust26@gmail.com>
2023-05-11 23:15:51 -0700

    End og class sync

---
+++ index.js
@@ -0,0 +1,14 @@
+/* 
+Visit google.com
+1. Change the button that says Google Search to "Froogle Search"
+2. Make the background color of the body your favorite color
+3. Replace the google logo image with another image link (Maybe use https://img.logoipsum.com/297.svg)
+4. Now move the logo to the very top of the screen while keeping it centered
+5. Delete the entire footer section at the bottom of the page where all the links another
+6. Locate the form element on the page. Using the box model view, what is the forms content width and height? What happens if you add a 500px margin to the top using the box model view?
+7. Move the header to where the footer section was before.
+8. Bonus - Add a div to the bottom of the page and add your favorite lorem ipsom text (bacon ipsum anyone? https://baconipsum.com/)
+9. Double Bonus - Do something else cool on the site. Get creative with it!!!
+10. Triple Bonus - Go do the same thing to another website. See if you can uncover any cool easter eggs in the code.
+
+*/
